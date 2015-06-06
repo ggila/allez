34f498142f3aafffbe8a91ca9dfaaebe7949a8c7
Gautier <gilabert.gautier@gmail.com>
2015-06-06 20:04:48 +0200

    cleaning

--- gradient_descent_v0.py
+++ gradient_descent_v0.py
@@ -35,20 +35,30 @@ ax.plot_surface(H, P, Z)
 # Cost Function
 #   J(wh, wp) = 1 / 2m * sum(((wh * hi + wp * pi - yi) ^ 2, i, 0, m - 1)
 #   where m is the number of given data, Xi/Yi the input/output of i-th data
+ax2 = fig.add_subplot(122, projection='3d')
+ax2.set_title('Cost function')
+
+m = len(hours)
+
+WP_init = 5 * np.random.rand()
+WH_init = 5 * np.random.rand()
+Z_init = 0
+for i in range(m):
+    Z_init += 1. / (2 * m) * (WH_init * hours[i] + WP_init * pauses[i] - results[i])**2
+ax2.scatter(WH_init, WP_init, Z_init, s=40, c='r', marker='o')
 
 WH = np.arange(-5, 5, 0.25)
 WP = np.arange(-5, 5, 0.25)
 WH, WP = np.meshgrid(WH, WP)
 
-m = len(hours)
 
 Z = np.zeros(np.shape(WP))
 for i in range(m):
     Z += 1. / (2 * m) * (WH * hours[i] + WP * pauses[i] - results[i])**2
 
-ax2 = fig.add_subplot(122, projection='3d')
-ax2.set_title('Cost function')
 ax2.plot_surface(WH, WP, Z, rstride=4, cstride=4, cmap=cm.coolwarm)
+
+print WP_init, WH_init, Z_init
 
 # We calculate the weight of F (i.e wh, wp) by finding the minimum of the error. We find minimum of J by gradient descent :
 # We repeateadly update weight :
