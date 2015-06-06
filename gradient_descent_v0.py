471d178ce0f4a7819eead6fb4b12f1cdf8ecf192
Gautier <gilabert.gautier@gmail.com>
2015-06-06 02:03:41 +0200

    set_anim

--- gradient_descent_v0.py
+++ gradient_descent_v0.py
@@ -21,8 +21,11 @@ fig = plt.figure()
 
 # Add data to figure
 H, P, Z = hours, pauses, results
-ax = fig.add_subplot(111, projection='3d')
+ax = fig.add_subplot(121, projection='3d')
 ax.scatter(H, P, Z, c='r', marker='o')
+ax.set_xlabel('hours')
+ax.set_ylabel('pauses')
+ax.set_zlabel('result')
 ax.plot_surface(H, P, Z)
 
 # We want a function F which, given x hours of work and y number of pauses as inputs, outputs an estimate grade
@@ -43,8 +46,9 @@ Z = np.zeros(np.shape(WP))
 for i in range(m):
     Z += 1. / (2 * m) * (WH * hours[i] + WP * pauses[i] - results[i])**2
 
-ax = fig.add_subplot(111, projection='3d')
-ax.plot_surface(WH, WP, Z, rstride=4, cstride=4, cmap=cm.coolwarm)
+ax2 = fig.add_subplot(122, projection='3d')
+ax2.set_title('Cost function')
+ax2.plot_surface(WH, WP, Z, rstride=4, cstride=4, cmap=cm.coolwarm)
 
 # We calculate the weight of F (i.e wh, wp) by finding the minimum of the error. We find minimum of J by gradient descent :
 # We repeateadly update weight :
@@ -53,7 +57,4 @@ ax.plot_surface(WH, WP, Z, rstride=4, cstride=4, cmap=cm.coolwarm)
 # wj = wj - alpha / m * sum(())
 
 # Display figure
-ax.set_xlabel('hours')
-ax.set_ylabel('pauses')
-ax.set_zlabel('result')
 plt.show()
