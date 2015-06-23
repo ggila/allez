859335686df5dd3ede51926ed5ce0647bd92f9ed
ggila <ggilaber@student.42.fr>
2015-06-23 22:08:34 +0200

    get_hours

--- scrap/get_hours.py
+++ scrap/get_hours.py
@@ -5,22 +5,27 @@ url = 'https://duoquadragintien.fr'
 
 fd=open('hours.txt', 'w')
 
-pattern = re.compile('<a class="project-item block-item"[^-]+-([^-]+)-d06">[^\n]+\n(<[^\n]+\n){3}<span[^>]+>in progress')
+pattern = re.compile('<td>Jun \d\d, 2015, \d:\d\d:\d\d [AP]M</td><td>([\d.]+)<')
 
 b = mechanize.Browser()
 b.open(url)
 b.select_form(nr=0)
-b['user[login]']='ggilaber'
-b['user[password]']='Valpara1s0'
+b['login']='ggilaber'
+b.submit()
+b.select_form(nr=0)
+b['password']=''
 b.submit()
 
-with open('./student.txt', 'r') as f:
-    for line in f:
-        fd = b.open(url2 + line)
-        r = re.finditer(pattern, fd.read())
-        print line[:-1]
-        if not r: fd_no.write(line)
-        else:
-            for elem in r:
-                print elem.group(1)
-                write_in_txt(elem.group(1), line)
+for piscine in ['php','cpp','ocaml','unity']:
+    with open('./' + piscine + '.txt', 'r') as f:
+        for line in f:
+            b.select_form(nr=0)
+            b['login'] = line[:-1]
+            data = b.submit().read()
+            r = re.finditer(pattern, data)
+            fd.write(line[:-1])
+            for i in range(8):
+                elem = next(r)
+                fd.write(' ' + r.group(1))
+
+fd.close()
