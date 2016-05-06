285a736f11af2006660cb3206471c447dfea974c
ggila <ggilaber@student.42.fr>
2016-05-06 17:46:25 +0200

    reorg all

--- numpy/interact.py
+++
@@ -1,9 +0,0 @@
-from IPython.display import display
-from IPython.html.widgets import interact
-import sympy
-
-sympy.init_printing(use_latex='mathjax')
-
-@interact
-def factor(n=10):
-    display(sympy.factor(x**n-1))
