6f8649caeebff38c6a42b019d4e695d1d3a48438
ggila <ggilaber@student.42.fr>
2016-08-31 22:25:40 +0200

    find circuit ok

--- graph.py
+++ graph.py
@@ -1,4 +1,6 @@
 
+import copy
+
 # node id is a dict key: it musts be hashable (__eq__ and __hash__ should be define)
 
 class graph:
@@ -10,7 +12,7 @@ class graph:
         return n in self.nodes
 
     def _getNode(self, nodeId):
-        ''' return node with nodeId (initialize new node if nodeId does not exist)'''
+        ''' return node with nodeId (initialize new node if nodeId does not exist) '''
         if nodeId not in self.nodes:
             self.nodes[nodeId] = {'neigh' : set(),
                                   'pos' : None}
@@ -27,4 +29,18 @@ class graph:
         n1, n2 = self._getNode(nId1), self._getNode(nId2)
         n1['neigh'].update((nId2,))
         n2['neigh'].update((nId1,))
+
+    def _findCircuit(self, start, way, remain):
+        neighbour = self.nodes[way[-1]]['neigh']
+        if not remain and start in neighbour:
+            yield way + [start]
+        for n in neighbour:
+            if n != start and n not in way:
+                yield from self._findCircuit(start, way + [n], remain - 1)
+
+    def findCircuit(self):
+        ''' genrator which yield circuit in random order '''
+        for k, v in self.nodes.items():
+            yield from self._findCircuit(k, [k], len(self.nodes) - 1)
+        yield []
 
